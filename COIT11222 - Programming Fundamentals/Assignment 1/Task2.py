9d2882c28ff2c324286629be29e902a51671b2b8
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-18 12:39:01 +1000

    Delete COIT11222 - Programming Fundamentals directory

--- COIT11222 - Programming Fundamentals/Assignment 1/Task2.py
+++
@@ -1,5 +0,0 @@
-max_marks: int = 65
-name: str = input("Enter the name of the student: ")
-mark = int(input("Enter the mark of " + name + " out of " + str(max_marks) + ": "))
-p: float=(mark*100)/max_marks
-print(name + " recieved " + str(round(p, 2)) + "% of the total marks.")
