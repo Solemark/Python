b5d6b0b2949e29ec9968b575dcd13432f1657656
Solemarc <62088534+Solemark@users.noreply.github.com>
2022-12-19 18:21:22 +1000

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,15 +1,15 @@
-max: int = 20
-min: int = 1
-fizz: int = 3
-buzz: int = 5
-output: str = ""
+def fizz_buzz(fizz: int, buzz: int, max: int) -> str:
+    output: str = "\b"
+    i: int = 1
+    while(i <= max):
+        if 0 == i % fizz:
+            output += "fizz"
+        if 0 == i % buzz:
+            output += "buzz"
+        if "z" != output[len(output)-1]:
+            output += str(i)
+        output += "\n"
+        i += 1
+    return output
 
-for x in range(min, max+1):
-    output = ""
-    if x%fizz==0:
-        output += "Fizz"
-    if x%buzz==0:
-        output += "Buzz"
-    if output=="":
-        output = x
-    print(output)
+print(fizz_buzz(3, 5, 20))
